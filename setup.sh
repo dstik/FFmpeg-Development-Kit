374475c8fa6dd829c91722eb11e1293c82dceac2
Dave Schatz <dave@trytape.com>
2018-01-30 10:37:48 -0800

    updated deps in setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ echo "Running setup."
 sudo apt update
 sudo apt-get --quiet --yes install build-essential git autoconf libtool pkg-config gperf gettext yasm python-lxml
 sudo apt-get --quiet --yes install zlibc zlib1g-dev zlib1g libfontconfig1-dev libx264-dev libpng12-dev libass-dev libavcodec-dev libavcodec-extra libavcodec-ffmpeg-extra56 libavformat-dev libavutil-dev libfreetype6-dev libexpat1-dev
-sudo apt-get --quiet --yes install unzip
+sudo apt-get --quiet --yes install unzip zip
+sudo apt-get --quiet --yes install automake cmake libsdl2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev mercurial texinfo wget
 
 wget https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip
 unzip android-ndk-r14b-linux-x86_64.zip -d ~
